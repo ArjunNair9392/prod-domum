186d9cb1a4a7b90fb637698afe815bbd44bbcd09
Arjun Nair <arjunnair9392@gmail.com>
2019-09-17 19:42:39 -0400

    Social feed in rootReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,5 +1,6 @@
 import authReducer from './authReducer';
 import signUpReducer from './singUpReducer';
+import socialFeedReducer from './socialFeedReducer';
 import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore';
 import { firebaseReducer } from 'react-redux-firebase';
@@ -7,6 +8,7 @@ import { firebaseReducer } from 'react-redux-firebase';
 const rootReducer = combineReducers({
   auth: authReducer,
   signUp: signUpReducer,
+  socialFeed: socialFeedReducer,
   firestore: firestoreReducer,
   firebase: firebaseReducer
 });
