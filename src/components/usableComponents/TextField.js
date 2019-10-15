3e8d100cdbf3adfd853110c86706a4f2b230b159
Amit Doshi <ad5418@rit.edu>
2019-10-14 21:56:57 -0400

    Usable TextField

--- src/components/usableComponents/TextField.js
+++ src/components/usableComponents/TextField.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 
+const CssTextField = withStyles({
+    root: {
+      '& label.Mui-focused': {
+        color: '#ffa726',
+      },
+      '& .MuiInput-underline:after': {
+        borderBottomColor: 'white',
+      },
+      '& .MuiOutlinedInput-root': {
+        '&.Mui-focused fieldset': {
+          borderColor: '#ffa726',
+        },
+      },
+    },
+  })(TextField);
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -24,19 +40,17 @@ const useStyles = makeStyles(theme => ({
 export default function OutlinedTextFields(props) {
   const classes = useStyles();
 
-//   const handleChange = name => event => {
-//     setValues({ ...values, [name]: event.target.value });
-//   };
 
   return (
-      <TextField
-
-        label="Name"
+      <CssTextField
+        id={props.id}
+        label={props.label}
         className={classes.textField}
-        //value={}
-        onChange={this.props.handleChange}
-        margin="normal"
-        variant="outlined"
+        value={props.value}
+        onChange={props.onEdit}
+        margin={props.margin}
+        variant={props.variant}
+        fullWidth = {props.fullWidth}
       />
   );
 }
